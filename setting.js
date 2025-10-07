a71a5df187885c2de224a1045b6997fb0b1cfd49
ikhlaqahmedbugti3 <ikhlaqahmedbugti3@gmail.com>
2025-10-07 11:33:57 +0500

    Update setting.js

--- setting.js
+++ setting.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
+  OWNER_NAME: 'Ikhlaq Ahmed', //your name
+  OWNER_NUMBER: '93724211356', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
